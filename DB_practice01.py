a988efdfaf299baeb8d132486296a1508e54ce0b
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-17 23:43:17 +0900

    top down

--- DB_practice01.py
+++ DB_practice01.py
@@ -2,13 +2,13 @@
 
 from sys import stdin
 
-def route(N, M):
+def route(N, M):  #top-down
     if N == -1 or M == -1:
-        return 0 
+        return 0
     return max(column[N][M] + route(N - 1, M), column[N][M] + route(N , M - 1), column[N][M] + route(N - 1 , M - 1))
 
 N, M = map(int, stdin.readline().split())
 
 column = [list(map(int, input().split())) for _ in range(N)]
 
-print(route(N - 1,M - 1))
+print(route(N - 1, M - 1))
