78bc0f61a0144c7c91aa7d5579254e4c382838bc
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-18 21:38:04 +0900

    bottom-up앙댕

--- DB_practice01.py
+++ DB_practice01.py
@@ -7,6 +7,7 @@ from sys import stdin
 def route(N, M):  
     if N == -1 or M == -1:
         return 0
+
     return max(column[N][M] + route(N - 1, M), column[N][M] + route(N , M - 1), column[N][M] + route(N - 1 , M - 1))
 
 N, M = map(int, stdin.readline().split())
--- DB_practice02.py
+++ DB_practice02.py
@@ -5,11 +5,12 @@ from sys import stdin
 #bottom-up
 
 def route(N, M):
-    
-    
+    candy = 0
+    for m in range(M):
+        candy += max(column[m + 1][m], column[m][m + 1], column[m + 1][m + 1])
 
+    return candy
+        
 N, M = map(int, stdin.readline().split())
-
 column = [list(map(int, stdin.readline().split())) for _ in range(N)]
-
-print(column)
+print(route(N, M))
