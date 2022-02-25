f66ccc7fa9058504b7aa0486fec2eae4b24e7e5c
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-26 01:36:49 +0900

    내일마저하자ㅠ

---
+++ Implementation/practice04.py
@@ -0,0 +1,61 @@
+#구현문제 게임 개발
+
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+
+A, B, d = map(int, stdin.readline().split())
+
+map = [list(map(int,stdin.readline().split())) for _ in range(N)]
+            #북 동 남 서
+compass = [0, 1, 2, 3]
+steps = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+count  = 1
+map[A][B] = 1
+while True:
+
+    for i in compass:
+        if i == d:
+            a, b = steps[i]
+            nA = A + a
+            nB = B + b
+
+            if d == 0:
+                d = 3
+            else:
+                d -= 1
+
+        if map[nA][nB] == 1:
+            continue
+        A = nA
+        B = nB
+        print(A, B)
+        if map[A-1][B] == 1 and map[A][B-1] == 1 and map[A+1][B] == 1 and map[A][B + 1] == 1:
+            break
+       
+
+print(count)
+
+
+        
+        
+
+
+
+    
+    
+
+    
+
+
+
+
+
+
+# 4 4
+# 1 1 0
+# 1 1 1 1
+# 1 0 0 1
+# 1 1 0 1
+# 1 1 1 1
+
