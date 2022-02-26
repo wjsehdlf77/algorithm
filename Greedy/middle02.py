1b89b4d643a449242277f8ad419bf7fd56da47fd
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-26 15:23:05 +0900

    답안지

--- Greedy/middle02.py
+++ Greedy/middle02.py
@@ -2,15 +2,28 @@
 
 from sys import stdin
 
+# data = [int(n) for n in str(stdin.readline().rstrip())]
+
+# data.sort()
+
+# total = 0
+
+# for i in data:
+    
+#     total = max(total + i, total * i)
+
+# print(total)
+
 data = [int(n) for n in str(stdin.readline().rstrip())]
 
-data.sort()
+result = data[0]
 
-total = 0
+for i in range(1, len(data)):
+    num = data[i]
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
 
-for i in data:
-    
-    total = max(total + i, total * i)
-
-print(total)
+print(result)
     
