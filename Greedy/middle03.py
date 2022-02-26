c7a7a17aa46c88ffd656272dc4e5e9e8ac93b8c3
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-26 19:22:16 +0900

    문자열 뒤집기

---
+++ Greedy/middle03.py
@@ -0,0 +1,30 @@
+#greedy 문자열 뒤집기
+
+from sys import stdin
+
+data = [int(n) for n in stdin.readline().rstrip()]
+
+count1 = 0
+count0 = 0
+
+if data[0] == 0:
+    count1 += 1
+else:
+    count0 += 1
+
+
+for i in range(len(data) - 1):
+    if data[i] != data[i + 1]:
+        if data[i + 1] == 1:
+            count0 += 1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
+
+
+
+
+    
+
+
