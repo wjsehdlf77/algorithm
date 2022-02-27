a0e2ced904bfef724f3bace628494463c0846b77
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-02-27 21:24:52 +0900

    답안방법

--- Greedy/middle5.py
+++ Greedy/middle5.py
@@ -1,20 +1,40 @@
 #greedy 볼링공 고르기
 
 from sys import stdin
+
+# N, M = map(int,stdin.readline().split())
+# K = list(map(int, stdin.readline().split()))
+
+# total = (len(K) * (len(K) - 1)) // 2
+# count = 0
+# K_SET = list(set(K))
+
+# for data in K_SET:
+#     num = K.count(data)
+#     if num > 1:
+#         count += num
+#         total -= (count * (count-1)) // 2
+#         count = 0
+# print(total)
+
+
+
 N, M = map(int,stdin.readline().split())
 K = list(map(int, stdin.readline().split()))
 
-total = (len(K) * (len(K) - 1)) // 2
-count = 0
-K_SET = list(set(K))
+array = [0] * 10
 
-for data in K_SET:
-    num = K.count(data)
-    if num > 1:
-        count += num
-        total -= (count * (count-1)) // 2
-        count = 0
-print(total)
+for x in K:
+    array[x - 1] += 1
+
+result = 0
+
+for i in range(M + 1):
+    N -= array[i]
+    result += array[i] * N
+
+print(result)
+
 
 
 # 5 3
