85e38302fe92b28058dbccd4d472af9f4fea1986
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-03-19 20:59:08 +0900

    시간오래걸림

--- 이진탐색/practice01-2.py
+++ 이진탐색/practice01-2.py
@@ -1,2 +1,37 @@
 #부품찾기 -> 계수정렬.ver
 
+
+#내방법
+from sys import stdin
+
+# N = int(stdin.readline())
+# sell_data = list(map(int, stdin.readline().split()))
+
+# M = int(stdin.readline())
+# buy_data = list(map(int, stdin.readline().split()))
+
+# count = [0] * (max(sell_data) + 1)    부품수는 1<=N<=1,000,000 이므로 저장공간은 1,000,001이다
+# count = [0] * 1000001
+
+# for i in range(len(sell_data)):
+#     count[sell_data[i]] += 1
+
+# for i in buy_data:
+#     if count[i] > 0:
+#         print('yes', end=' ')
+#     else:
+#         print('no', end=' ')
+
+N = int(stdin.readline())
+array = [0] * 1000001
+for i in input().split():
+    array[int(i)] = 1
+
+M = int(stdin.readline())
+buy_data = list(map(int, stdin.readline().split()))
+
+for i in buy_data:
+    if array[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
---
+++ 이진탐색/practice02.py
@@ -0,0 +1,64 @@
+#떡볶이 떡 만들기
+
+from sys import stdin
+
+#내방식은 끝에서 부터 -1씩 줄여가면서 탐색하므로 이진탐색으로 반반씩 줄여가는 것에 비해 시간이 훨씬 오래 걸릴것이다
+
+# def max_height(array, max, client):
+#     lambda_height = lambda x : x- max if x > max else 0
+#     sum_height = sum(map(lambda_height, array))
+    
+#     if sum_height >= client:
+#         return max
+#     else:
+#         return max_height(array, max - 1, client)
+    
+
+# N, M = map(int, stdin.readline().split())
+
+# data = list(map(int, stdin.readline().split()))
+
+# data.sort()
+
+# max_data = max(data)
+
+# print(max_height(data, max_data, M))
+
+
+def max_height(array, start, end, client):
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+
+    lambda_height = lambda x : x- mid if x > mid else 0
+    sum_height = sum(map(lambda_height, array))
+    
+    if sum_height == client:
+        return mid
+    elif sum_height > client:
+        return max_height(array, mid + 1, end, client)
+    else:
+        return max_height(array, start, mid - 1, client)
+    
+
+N, M = map(int, stdin.readline().split())
+
+data = list(map(int, stdin.readline().split()))
+
+data.sort()
+
+print(max_height(data, 0, max(data), M))
+
+
+
+
+
+
+
+
+
+# 4 6
+# 19 15 10 17
+
+# 10 20
+# 23 12 24 21 30 5 2 7 15 17
