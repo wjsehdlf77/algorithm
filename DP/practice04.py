59c17d583f41fd8b93b28eb81ddd09aebbb7674d
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-03-21 20:31:31 +0900

    안해

---
+++ DP/practice04.py
@@ -0,0 +1,19 @@
+#효율적인 화폐 구성
+
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+
+data = [int(n) for n in stdin.readline().rstrip()]
+
+
+
+# 2 15
+# 2
+# 3
+
+# 3 4
+# 3
+# 5
+# 7
+
