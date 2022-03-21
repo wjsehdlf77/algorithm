56388e51c754f308e3938b02d539eea7269a1e52
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-03-21 20:23:49 +0900

    dp is hard

--- DP/practice03.py
+++ DP/practice03.py
@@ -4,5 +4,14 @@ from sys import stdin
 
 N = int(stdin.readline())
 
-d = [0] * 1001
+d = [0] * 10
 
+d[1] = 1
+d[2] = 3
+
+for i in range(3, N + 1): 
+
+    d[i] = (d[i - 1] + d[i - 2] * 2) % 796796
+    print(d)
+
+print(d[N])
