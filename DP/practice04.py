5ee9840e6e5f9f6375f1e72774426694c09a6d12
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-03-26 22:07:00 +0900

    본인꺼 수정

--- DP/practice04.py
+++ DP/practice04.py
@@ -4,29 +4,6 @@ from sys import stdin
 
 
 # 내방법
-# N, M = map(int, stdin.readline().split())
-
-# d = [10001] * (M + 1)
-# d[0] = 0
-
-# data = []
-
-# for _ in range(N):
-#     data.append(int(stdin.readline()))
-
-# for i in data:
-#     for j in range(0, M + 1):
-#         if d[j] > 0:
-#             d[j] = min(d[j], d[j - i] + 1)
-#             print(d[i - i] + 1, end = ', ')
-            
-
-# if d[M] == 10001:
-#     print(-1)
-# else:
-#     print(d[15])
-
-
 N, M = map(int, stdin.readline().split())
 
 d = [10001] * (M + 1)
@@ -37,15 +14,38 @@ data = []
 for _ in range(N):
     data.append(int(stdin.readline()))
 
-for i in range(N):
-    for j in range(data[i], M + 1):         #데이터의 순서에 따라 그 값에 맞게 루프문이 시작한다
-        if d[j - data[i]] != 10001:
-            d[j] = min(d[j], d[j - data[i]] + 1)
+for i in data:
+    for j in range(i, M + 1):
+        if d[j - i] != 10001:
+            d[j] = min(d[j], d[j - i] + 1)
+    print(d)
+            
 
 if d[M] == 10001:
     print(-1)
 else:
-    print(d[M])
+    print(d[15])
+
+
+# N, M = map(int, stdin.readline().split())
+
+# d = [10001] * (M + 1)
+# d[0] = 0
+
+# data = []
+
+# for _ in range(N):
+#     data.append(int(stdin.readline()))
+
+# for i in range(N):
+#     for j in range(data[i], M + 1):         #데이터의 순서에 따라 그 값에 맞게 루프문이 시작한다
+#         if d[j - data[i]] != 10001:
+#             d[j] = min(d[j], d[j - data[i]] + 1)
+
+# if d[M] == 10001:
+#     print(-1)
+# else:
+#     print(d[M])
 
 
 
