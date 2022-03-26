a2373e99226fd6098142df2d71a84a41aab50c4a
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-03-26 21:57:33 +0900

    일단 성공

--- DP/practice04.py
+++ DP/practice04.py
@@ -2,11 +2,41 @@
 
 from sys import stdin
 
+
+# 내방법
 N, M = map(int, stdin.readline().split())
 
-data = [int(n) for n in stdin.readline().rstrip()]
+d = [10001] * (M + 1)
+d[0] = 0
+
+data = []
+
+for _ in range(N):
+    data.append(int(stdin.readline()))
+
+for i in data:
+    for j in range(0, M + 1):
+        if d[j] > 0:
+            d[j] = min(d[j], d[j - i] + 1)
+            print(d[i - i] + 1, end = ', ')
+            
+
+if d[M] == 10001:
+    print(-1)
+else:
+    print(d[15])
 
 
+
+
+
+
+
+
+
+
+# 1 15
+# 2
 
 # 2 15
 # 2
