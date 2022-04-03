e88332c2f25f49fef55e16ebab6390bc439f300d
Do Il Jeon <98448316+wjsehdlf77@users.noreply.github.com>
2022-04-03 15:40:02 +0900

    플로이드워셜방법

--- Shortest Path/practice01.py
+++ Shortest Path/practice01.py
@@ -6,7 +6,60 @@
 
 
 #내가 푼 다익스트라 방법
-import heapq
+# import heapq
+# from sys import stdin
+
+# input = stdin.readline
+
+# INF = int(1e9)
+
+# n, m = map(int, input().split())
+
+# graph = [[] for _ in range(n + 1)]
+
+# distance = [INF] * (n + 1)
+
+# for _ in range(m):
+#     route1, route2 = map(int, input().split())
+#     if route2 not in graph[route1]: 
+#         graph[route1].append(route2)
+#     if route1 not in graph[route2]: 
+#         graph[route2].append(route1)
+
+
+# x, k = map(int, input().split())
+
+
+# def company_route(start = 1):
+#     q = []
+#     heapq.heappush(q, (0, start))
+#     distance[start] = 0
+    
+#     while q:
+#         dist, now = heapq.heappop(q)
+#         if dist > distance[now]:
+#             continue
+
+#         for i in graph[now]:
+#             cost = dist + 1
+
+#             if cost < distance[i]:
+#                 distance[i] = cost
+#                 heapq.heappush(q, (cost, i))
+
+
+# company_route()
+# first = distance[k]
+# company_route(k)
+# end = distance[x]
+
+# if  (first + end) < INF:
+#     print(first + end)
+# else:
+#     print(-1) 
+
+#플로이드 워셜 방법
+
 from sys import stdin
 
 input = stdin.readline
@@ -15,53 +68,32 @@ INF = int(1e9)
 
 n, m = map(int, input().split())
 
-graph = [[] for _ in range(n + 1)]
-
-distance = [INF] * (n + 1)
+graph = [[INF]*(n + 1) for _ in range(n + 1)]
 
 for _ in range(m):
-    route1, route2 = map(int, input().split())
-    if route2 not in graph[route1]: 
-        graph[route1].append(route2)
-    if route1 not in graph[route2]: 
-        graph[route2].append(route1)
+    a, b = map(int, input().split())
 
+    graph[a][b] = 1
+    graph[b][a] = 1
 
 x, k = map(int, input().split())
 
 
-def company_route(start = 1):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    
-    while q:
-        dist, now = heapq.heappop(q)
-        if dist > distance[now]:
-            continue
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
 
-        for i in graph[now]:
-            cost = dist + 1
-
-            if cost < distance[i]:
-                distance[i] = cost
-                heapq.heappush(q, (cost, i))
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
 
-company_route()
-first = distance[k]
-company_route(k)
-end = distance[x]
-
-if  (first + end) < INF:
-    print(first + end)
+if(graph[1][k] + graph[k][x]) < INF:
+    print(graph[1][k] + graph[k][x])
 else:
-    print(-1) 
-
-
-
-
-
+    print(-1)
 
 
 
